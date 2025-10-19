f8769355f092934054762779cc8489ba3d34d2b2
Tuna Yaylali <go56fup@mytum.de>
2025-10-19 03:19:15 +0200

    Add Nix/envrc devshell

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "Dev shell for MolSim Praktikum";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+
+        llvm = pkgs.llvmPackages_20;
+      in
+      {
+        devShells.default = pkgs.mkShell.override { stdenv = llvm.libcxxStdenv; } {
+          packages = with pkgs; [
+            llvm.libcxxClang
+            llvm.llvm
+            gcc15
+            cmake
+            doxygen
+            paraview
+            vtk
+            cmake-format
+          ];
+        };
+      }
+    );
+}
