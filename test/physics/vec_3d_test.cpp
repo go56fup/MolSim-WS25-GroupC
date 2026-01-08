ef49d73f0f2767975a719973082b0380f852ca82
Melisande Walcher <melisandewalcher@MacBook-Air-de-Melisande.local>
2026-01-08 20:01:35 +0100

    added thermostat tests

--- test/physics/vec_3d_test.cpp
+++ test/physics/vec_3d_test.cpp
@@ -4,7 +4,10 @@
 #include "gtest_constexpr/macros.hpp"
 #include "testing_utilities.hpp"
 
+#include "grid/particle_container/fwd.hpp"
+#include "physics/thermostat.hpp"
 #include "physics/vec_3d.hpp"
+#include "simulation/molsim.hpp"
 
 // NOLINTBEGIN(*magic-numbers)
 
@@ -81,5 +84,82 @@ TEST(VectorTests, Equal) {
 	GTEST_CXP vec b{1, 2, 3};
 	STATIC_EXPECT_EQ(a, b);
 }
+
+TEST(ThermostatTests, Cooling) {
+	GTEST_CXP double init_temp = 40;
+	GTEST_CXP double target_temp = 30;
+	GTEST_CXP double mass = 2.0;
+	GTEST_CXP double brownian_mean = std::sqrt(init_temp/ mass);
+	GTEST_CXP particle_container container(vec{2, 2, 2}, 1);
+	GTEST_CXP thermostat_parameters therm = {init_temp, 2, target_temp, std::numeric_limits<double>::infinity()};
+	GTEST_CXP size_t seq = 2;
+
+	container.add_cuboid<2>({0,0,0}, {1,1,0}, 1.1225, {0,0,0}, 2.0, 1.0, 5.0, brownian_mean, seq);
+	run_thermostat(container, therm, 2);
+
+	double resulting_temp = 0;
+	for (const particle& p : container.particles()) {
+		const double squared_norm = (p.v.x * p.v.x) + (p.v.y * p.v.y) + (p.v.z * p.v.z);
+		resulting_temp += p.m * squared_norm;
+	}
+	resulting_temp /= 2 * static_cast<double>(container.size());
+	STATIC_EXPECT_EQ(resulting_temp, target_temp);
+}
+
+TEST(ThermostatTests, Heating) {
+	GTEST_CXP double init_temp = 30;
+	GTEST_CXP double target_temp = 40;
+	GTEST_CXP double mass = 2.0;
+	GTEST_CXP double brownian_mean = std::sqrt(init_temp/ mass);
+	GTEST_CXP particle_container container(vec{2, 2, 2}, 1);
+	GTEST_CXP thermostat_parameters therm = {init_temp, 2, target_temp, std::numeric_limits<double>::infinity()};
+	GTEST_CXP size_t seq = 2;
+
+	container.add_cuboid<2>({0,0,0}, {1,1,0}, 1.1225, {0,0,0}, 2.0, 1.0, 5.0, brownian_mean, seq);
+	run_thermostat(container, therm, 2);
+
+	double resulting_temp = 0;
+	for (const particle& p : container.particles()) {
+		const double squared_norm = (p.v.x * p.v.x) + (p.v.y * p.v.y) + (p.v.z * p.v.z);
+		resulting_temp += p.m * squared_norm;
+	}
+	resulting_temp /= 2 * static_cast<double>(container.size());
+	STATIC_EXPECT_EQ(resulting_temp, target_temp);
+}
+
+TEST(ThermostatTests, Holding) {
+	GTEST_CXP double init_temp = 40;
+	GTEST_CXP double mass = 2.0;
+	GTEST_CXP double brownian_mean = std::sqrt(init_temp/ mass);
+	GTEST_CXP particle_container container(vec{2, 2, 2}, 1);
+	GTEST_CXP thermostat_parameters therm = {init_temp, 2, init_temp, std::numeric_limits<double>::infinity()};
+	GTEST_CXP size_t seq = 2;
+	GTEST_CXP auto config = sim_configuration{
+		.delta_t = 0.1,
+		.cutoff_radius = 1,
+		.boundary_behavior = boundary_conditions_descriptor::all(boundary_condition::outflow),
+		.end_time = 10,
+		.write_frequency = 1,
+		.base_name{std::from_range, "unused"},
+		.domain{2, 2, 2},
+		.create_checkpoint = false,
+		.dimensions = 2
+	};
+
+	container.add_cuboid<2>({0,0,0}, {1,1,0}, 1.1225, {0,0,0}, 2.0, 1.0, 5.0, brownian_mean, seq);
+	for (int i = 0; i< 10; i++) {
+		run_thermostat(container, therm, 2);
+		run_sim_iteration(lennard_jones_force, container, config);
+	}
+
+	double resulting_temp = 0;
+	for (const particle& p : container.particles()) {
+		const double squared_norm = (p.v.x * p.v.x) + (p.v.y * p.v.y) + (p.v.z * p.v.z);
+		resulting_temp += p.m * squared_norm;
+	}
+	resulting_temp /= 2 * static_cast<double>(container.size());
+	STATIC_EXPECT_EQ(resulting_temp, init_temp);
+}
+
 
 // NOLINTEND(*magic-numbers)
