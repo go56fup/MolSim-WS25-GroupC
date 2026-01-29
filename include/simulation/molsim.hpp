64e7d551217ddb26ae1258b5a5ea732eb41efe78
Gabriel Ribeiro Fernandes <go98pal@mytum.de>
2026-01-29 16:21:43 +0100

    logging thread num

--- include/simulation/molsim.hpp
+++ include/simulation/molsim.hpp
@@ -54,6 +54,8 @@ constexpr void calculate_forces_batched(particle_container& container) noexcept 
 		particle_batch batch_p1;
 		particle_batch batch_p2;
 
+		spdlog::info("Thread number {}", get_num_threads());
+
 		for (auto [p1_idx, p2_idx]: unique_pairs(cell)) {
 			TRACE_FORCES(
 					"Putting {} {} into batch, count={} on thread {}", p1_idx, p2_idx, count,
