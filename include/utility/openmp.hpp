1eb1aae4ea3c573dfa51ae3ce6d47f8a33bd139b
Gabriel Ribeiro Fernandes <go98pal@mytum.de>
2026-01-29 15:24:48 +0100

    logging thread num

--- include/utility/openmp.hpp
+++ include/utility/openmp.hpp
@@ -38,13 +38,9 @@ inline void set_num_threads() {
 
 
 inline void debug_omp() {
-#if SINGLETHREADED
-	return;
-#else
 	spdlog::info("--- OpenMP Debug ---");
 	spdlog::info("Max threads (omp_get_max_threads): {}", get_max_threads());
 	spdlog::info("Thread limit (omp_get_thread_limit): {}", omp_get_thread_limit());
 	spdlog::info("Nested level (omp_get_level): {}", omp_get_level());
 	spdlog::info("Is dynamic adjustment enabled: {}", omp_get_dynamic());
-#endif
 }
